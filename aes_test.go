81d22b1251c71aa9bfb6fb451acdfce02c891cc3
do-nothing <lj110119@aliyun.com>
2018-06-06 16:17:44 +0800

    add Decode test.

--- aes_test.go
+++ aes_test.go
@@ -16,6 +16,8 @@ func TestAes(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println(base64.StdEncoding.EncodeToString(result))
+
+	result, err = base64.StdEncoding.DecodeString("xyUdDpguOmAfQoCFeKWJnQ+o/RxkH0IctvuIADUug/w=")
 	origData, err := AesDecrypt(result, key)
 	if err != nil {
 		panic(err)
